bd8e6b530501819c94eb9e2c77b0d003187a0ee4
Uchenna David <uchennadavid153@gmail.com>
2021-09-10 16:49:15 +0100

    added transaction end point

--- Controllers/TransactionController.js
+++ Controllers/TransactionController.js
@@ -1,12 +1,15 @@
 class TransactionController{
     
     buy(req, res, next){
-        (!req.params.id)?res.json({message:'Tramsaction Failed'}):res.json({message:'Successful Transaction'})
+        (!req.params.id)?res.json({message:'Tramsaction Failed '}):res.json({message:'Successful Transaction'})
         next()
     }
+    
     sell(req, res, next){
         (!req.params.id)?res.json({message:'Tramsaction Failed'}):res.json({message:'Successful Transaction'})
         next()
     }
+
 }
+
 module.exports = new TransactionController()
