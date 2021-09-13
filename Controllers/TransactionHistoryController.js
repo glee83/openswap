0ade9ae40c6cac82cadd6966d36b573d94cde908
Glee Albert <gleealbert307@gmail.com>
2021-09-13 15:42:48 +0100

    transaction history

---
+++ Controllers/TransactionHistoryController.js
@@ -0,0 +1,15 @@
+class HistoryController {
+    history(req, res, next) {
+        
+        res.json(
+            { 
+                user: 'Glee', 
+                transactionType: '', 
+                transactionTime: [], 
+                paymenMethod: ''  
+            }
+        );
+    }
+}
+
+module.exports = new HistoryController()
--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var PublicController = require('../Controllers/PublicController');
 var UserController = require('../Controllers/UserController');
 var TradeController = require('../Controllers/TradeController');
+const TransactionHistory = require('../Controllers/TransactionHistoryController')
 
 router.get('/', PublicController.index);
 
@@ -13,5 +14,6 @@ router.get('/users/register', UserController.register);
 /* Trade Routes */
 router.get('/trades', TradeController.index);
 router.post('/trade', TradeController.save);
+router.get('/history', TransactionHistory.history)
 
 module.exports = router;
